58d6998369df045bfdb8933acc824adfbce4d482
dehanalkautsar <dehanalkautsar3@gmail.com>
2020-11-09 10:23:42 +0700

    add stemming
    masih kek yg basic bgt gitu

---
+++ stemmy.py
@@ -0,0 +1,11 @@
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+with open('data1.txt', 'r') as file:            #nama file data1 yang nantinya bakal diganti
+    data = file.read().replace('\n', '')
+
+output = stemmer.stem(data)
+
+print(output)
