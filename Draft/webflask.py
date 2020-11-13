b7ad67840887a66bcc8455a84be7b69b3a1a0319
irvinandryan <irvin.andryan@gmail.com>
2020-11-13 23:56:57 +0700

    fix route mainpage-perihal

--- Draft/webflask.py
+++ Draft/webflask.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask, render_template, flash, request
-app = Flask(__name__)
+from flask import Flask, render_template, flash, request, url_for, redirect
+from werkzeug.utils import secure_filename
 
-@app.route('/')
+UPLOAD_FOLDER = '/uploadFiles'
+ALLOWED_EXTENSIONS = {'txt','pdf'}
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/') #good
 def mainPage():
     return render_template('mainpage.html')
 
-@app.route('/Upload')
+@app.route('/Perihal') #good
+def perihal():
+    return render_template('perihal.html')
+
+@app.route('/Upload', methods = ['GET','POST']) #masih bad
 def uploadPage():
     return ("ini page buat upload")
 
