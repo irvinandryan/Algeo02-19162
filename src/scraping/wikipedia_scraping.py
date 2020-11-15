f912cd512f8b4ecb438c88a0b9a7e787f97bebde
abiwardani <abiwardani85@gmail.com>
2020-11-15 19:39:39 +0700

    fix directory scraping

--- src/scraping/wikipedia_scraping.py
+++ src/scraping/wikipedia_scraping.py
@@ -1,6 +1,4 @@
 import wikipedia
-from bs4 import BeautifulSoup
-import requests
 import numpy as np
 
 def search(query, search_size=1):
@@ -11,7 +9,7 @@ def search(query, search_size=1):
     for title in titles:
         document = ""
         document += wikipedia.summary(title)
-        filename = "Data/"+title.replace(" ", "_")+".txt"
+        filename = ".../test/"+title.replace(" ", "_")+".txt"
         f = open(filename, "w")
         f.write(document)
         f.close()
