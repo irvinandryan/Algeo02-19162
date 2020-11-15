35e00eab869f8ac3cfa131c4c8aa89498915dbae
Muhammad Rifat Abiwardani <50320606+abiwardani@users.noreply.github.com>
2020-11-15 19:22:31 +0700

    apus draft

--- TabTerm.py
+++
@@ -1,6 +0,0 @@
-from stemmy import output #ini nanti outputnya diganti buat beberapa file
-TabTerm = []
-for i in output.split():
-    if not i in TabTerm:
-        TabTerm.append(i)
-print(TabTerm)
